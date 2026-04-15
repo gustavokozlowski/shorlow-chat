8bbae3abf8c0c82211061d1ada972e9f26245eb0
gustavokozlowski <guskozlowskidev@gmail.com>
2026-04-15 09:49:30 -0300

    feat(user.controller): add interface to create-user request and response

---
+++ src/controllers/user.controller.types.ts
@@ -0,0 +1,11 @@
+import type { User } from "../models/user/user.interface";
+
+export interface CreateUserRequest  {
+  userData: Omit<User, "_id">
+}
+
+export interface CreateUserResponse {
+  message: string
+  data: Omit<User, "_id">
+  
+}
