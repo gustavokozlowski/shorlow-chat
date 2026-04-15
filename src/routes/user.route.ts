8e3596c772b1d43c65eef0d2fa4c61ab21b72c3a
gustavokozlowski <guskozlowskidev@gmail.com>
2026-04-15 09:59:54 -0300

    feat(route): add route to create user

---
+++ src/routes/user.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import userController from "../controllers/user.controller";
+
+const userRoute = Router();
+
+userRoute.post('/create', userController.createUser );
+
+export default userRoute
+
