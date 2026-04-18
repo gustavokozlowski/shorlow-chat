86219df58d249ac9a7edba780e5fc0df810b7cbd
gustavokozlowski <guskozlowskidev@gmail.com>
2026-04-17 21:46:50 -0300

    feat: add message controller send method

---
+++ src/controllers/message/message.controller.types.ts
@@ -0,0 +1,12 @@
+import type { Message } from '../../models/message/message,models.interface'
+
+type messageData = Omit<Message, 'createdAt'>
+
+export interface SendMessageRequest {
+    message: messageData;
+}
+
+export interface SendMessageResponse {
+    success: boolean
+    message: string;
+}
