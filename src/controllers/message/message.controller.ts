ce18c982b3e2e9171087ac66bee1d78453e7d96a
gustavokozlowski <guskozlowskidev@gmail.com>
2026-04-18 23:04:45 -0300

    remove: console.info

--- src/controllers/message/message.controller.ts
+++ src/controllers/message/message.controller.ts
@@ -1,6 +1,5 @@
 import type { Request, Response } from 'express';
 import messageModel from '../../models/message/message.model';
-
 class MessageController {
 	public async send(req: Request, res: Response) {
 		const { content } = req.body;
@@ -10,7 +9,7 @@ class MessageController {
 			sender: req.user?._id.toString(),
 			receiver: req.params.id?.toString(),
 		});
-
+		
 		return res.status(201).json({
 			success: true,
 			data: message,
