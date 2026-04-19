379082907e937672235b1506e8095943d02f0863
gustavokozlowski <guskozlowskidev@gmail.com>
2026-04-18 23:05:17 -0300

    style: biome check

--- src/controllers/message/message.controller.ts
+++ src/controllers/message/message.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express';
 import messageModel from '../../models/message/message.model';
+
 class MessageController {
 	public async send(req: Request, res: Response) {
 		const { content } = req.body;
@@ -9,7 +10,7 @@ class MessageController {
 			sender: req.user?._id.toString(),
 			receiver: req.params.id?.toString(),
 		});
-		
+
 		return res.status(201).json({
 			success: true,
 			data: message,
