db372e12e8468445e5aaeef612954bebef712209
gustavokozlowski <guskozlowskidev@gmail.com>
2026-04-18 21:25:37 -0300

    improve(route): add auth middleware on message

--- src/routes/message.route.ts
+++ src/routes/message.route.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import messageController from '../controllers/message/message.controller';
+import authMiddleware from '../middlewares/auth.middleware';
 
 const messageRoute = Router();
 
-messageRoute.post('/send/:id', messageController.send);
+messageRoute.post('/send/:id',
+ authMiddleware.authUserByToken,
+ messageController.send);
 
 export default messageRoute;
