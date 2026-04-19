5b91eb6cd38aa5ab9dc559f7f5593de2196cb1cd
gustavokozlowski <guskozlowskidev@gmail.com>
2026-04-18 21:03:54 -0300

    improve: add global enums to centralize msg

---
+++ src/global.enums.ts
@@ -0,0 +1,8 @@
+export enum AUTH_MESSAGE {
+    ERROR = 'Acesso negado, token não encontrado.\nRealize o login e tente novamente',
+    INVALID = 'Token inválido!'
+}
+
+export enum USER_MESSAGE {
+    NOTFOUND = 'Usuário não existe no banco de dados!'
+}
