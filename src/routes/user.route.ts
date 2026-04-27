527f345a0ee3c1a82ec07d94bc8c56a4678ad672
gustavokozlowski <guskozlowskidev@gmail.com>
2026-04-26 22:47:32 -0300

    feat:  create route to get user by id

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import userController from '../controllers/user/user.controller';
+import authMiddleware from '../middlewares/auth.middleware';
 
 const userRoute = Router();
 
 userRoute.post('/register', userController.register);
 userRoute.post('/login', userController.login);
+
+userRoute.get(
+	'/:id',
+	authMiddleware.authUserByParams,
+	authMiddleware.authUserByToken,
+	userController.getById,
+);
 
 export default userRoute;
